35d6d5b4c2d1232ec7b238a4222bfbaefccdbc8d
Pamela Frieze <pfrieze@gmail.com>
2017-11-23 15:02:31 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,5 +2,6 @@ require_relative "./vehicle.rb"  #this class needs access to the vehicle class
 #avoid repititious code
 class Car < Vehicle  #inherit the car class from vehicle
   def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
 end
